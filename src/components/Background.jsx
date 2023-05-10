b16ed4a1314018417f4d36101123f6ec9004ec35
Linus Shaddai <awajisomegak@gmail.com>
2023-05-10 21:51:39 +0100

    Refactor code

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -7,12 +7,11 @@ const Background = () => {
         <h2>Background</h2>
         <div className='display-background'>
             <div className="background-text">
-                <p className='first-p'>I love creating user-friendly websites and adding creative elements to make them more memorable. </p>
-                <p>I have always been interested in web development, so I decided to pursue a career in the tech industry. I originally majored in front end developemt, but my interests soon turned to back-end devlopement . I am looking forward to seeing where it takes me and how much i can achieve.</p>
+                <p>Linus Shaddai loves creating user-friendly websites and adding creative elements to make them more memorable. 
 
-                <p>I am most excited by the different animations and interactions I can bring to a website, designing static stuff is just not my thing. I have only recently discovered webgl and am looking forward to learning more about it and experimenting with different ways of interacting with the user in the future!</p>
+                She is most excited by the different animations and interactions she can bring to a website. She loves to collaborate and peer program as it opens her up to other perspective of writing code.
 
-                <p>I am an easy going person who enjoys spending time with good people. I love watching movies, playing videogames, and going out to eat when I am not working. I am extremely motivated to learn and grow as a developer.</p>
+                She is an easy going person who enjoys spending time with good people. She loves to see movies, make music, and going out to eat when she's not working.</p>
             </div>
             <div className='img-div'>
                 <img src={Photo} alt="" />
